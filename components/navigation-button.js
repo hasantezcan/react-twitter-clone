c704be2154890e8b4aea5149740a39c20a051e04
hasantezcan <hasantezcan77@gmail.com>
2020-06-22 23:28:39 +0300

    Add notification badge

--- components/navigation-button.js
+++ components/navigation-button.js
@@ -5,13 +5,14 @@ import Button from './button'
 
 import styles from './navigation-button.module.css'
 
-function NavigationButton({ selected, children, ...props }) {
+function NavigationButton({ notify, selected, children, ...props }) {
   return (
     <Button
       className={cn(styles.navButton, selected && styles.navButtonSelected)}
       {...props}
     >
       {children}
+      {notify && <span className={styles.notify}> {notify} </span>}
     </Button>
   )
 }
--- components/navigation.js
+++ components/navigation.js
@@ -34,7 +34,7 @@ function Navigation({ selectedKey }) {
         <TitleBold>Explore</TitleBold>
       </NavigationButton>
 
-      <NavigationButton selected={selectedKey === 'notification'}>
+      <NavigationButton notify={17} selected={selectedKey === 'notification'}>
         <Notification />
         <TitleBold>Notification</TitleBold>
       </NavigationButton>
