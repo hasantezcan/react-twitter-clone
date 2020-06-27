9c116a72fb9210dfb948cfe3a4f3344d2c737b9c
hasantezcan <hasantezcan77@gmail.com>
2020-06-27 22:15:23 +0300

    Create typography style guide

---
+++ stories/3-Typography.stories.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import TextTitle from '../components/text-title'
+
+export default {
+  title: 'Typography'
+}
+
+export const Typography = () => (
+  <div>
+    <TextTitle>Merhaba Dünya</TextTitle>
+    <TextTitle>Merhaba Dünya</TextTitle>
+  </div>
+)
