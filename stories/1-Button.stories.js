b864fbdf8a250e041716f09c28fcd0e0bfa840e4
hasantezcan <hasantezcan77@gmail.com>
2020-06-30 16:19:26 +0300

    Create theme button component

--- stories/1-Button.stories.js
+++ stories/1-Button.stories.js
@@ -5,12 +5,21 @@ import NavigationButton from '../components/navigation-button'
 import Navigation from '../components/navigation'
 import { Home } from '../components/icons'
 import TextTitle from '../components/text-title'
+import ThemeButton from '../components/theme-button'
+import Stack from '../components/stack'
 
 export default {
   title: 'Buttons'
 }
 
 export const Normal = () => <Button>Save</Button>
+export const Theme = () => (
+         <div>
+           <ThemeButton>Tweet</ThemeButton>
+           <ThemeButton full>Tweet Full</ThemeButton>
+           <ThemeButton full big>Tweet Big Full</ThemeButton>
+         </div>
+       )
 
 export const NavButton = () => (
   <NavigationButton>
