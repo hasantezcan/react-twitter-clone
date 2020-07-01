398df4a0309375485092742d68e2417e0ecb163b
hasantezcan <hasantezcan77@gmail.com>
2020-07-01 10:58:04 +0300

    Create 'profile-box' component

---
+++ components/profile-box.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import cn from 'classnames'
+
+import styles from './profile-box.module.css'
+import { ArrowBottom } from './icons'
+
+import Photo from './photo'
+import Button from './button'
+import TextBody from './text-body'
+
+function ProfileBox({ name = 'Hasan Tezcan', slug = 'hasantezcann' }) {
+  return (
+    <Button className={cn(styles.box)}>
+      <Photo />
+      <div className={styles.body}>
+        <TextBody bold>{name}</TextBody>
+        <TextBody className={styles.slug}>@{slug}</TextBody>
+      </div>
+      <ArrowBottom className={styles.icon} />
+    </Button>
+  )
+}
+
+export default ProfileBox
