0cf5a3cf8d5f4098fe0c483069061608bb02ad87
hasantezcan <hasantezcan77@gmail.com>
2020-07-06 13:46:41 +0300

    Add useWindowsSize hook

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import useWindowSize from '../components/hooks/useWindowsSize'
 
 function HomePage() {
+  const size = useWindowSize();
+
   return (
     <Layout>
-      <div>sidebar</div>
-      <div>main</div>
-      <div>extra</div>
+      <div flat={size.width < 1270 }>sidebar</div>
+      <div>{JSON.stringify(size)}</div>
+      {size.width > 980 && <div>extra</div>}
     </Layout>
   )
 }
