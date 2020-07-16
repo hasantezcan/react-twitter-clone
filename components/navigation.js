0f27770b834b58768186655baefb724d8d6ae827
hasantezcan <hasantezcan77@gmail.com>
2020-07-16 03:21:17 +0300

    Add selected icons feature

--- components/navigation.js
+++ components/navigation.js
@@ -12,61 +12,77 @@ import {
   Bookmark,
   Lists,
   Profile,
-  More
+  More,
+  HomeFill,
+  ExplorerFill,
+  NotificationFill,
+  MessagesFill,
+  BookmarkFill,
+  ListsFill,
+  ProfileFill
 } from './icons'
 
 const MENU = [
   {
     key: 'twitter',
     icon: <Twitter />,
+    iconSelected: <Twitter />,
     title: '',
     notify: 0
   },
   {
     key: 'home',
     icon: <Home />,
+    iconSelected: <HomeFill />,
     title: 'Home',
     notify: 0
   },
   {
     key: 'explore',
     icon: <Explore />,
+    iconSelected: <ExplorerFill />,
     title: 'Explore',
     notify: 0
   },
   {
     key: 'notification',
     icon: <Notification />,
+    iconSelected: <NotificationFill />,
     title: 'Notification',
     notify: 17
   },
   {
     key: 'messages',
     icon: <Messages />,
+    iconSelected: <MessagesFill />,
     title: 'Messages',
     notify: 0
   },
   {
     key: 'bookmark',
     icon: <Bookmark />,
+    iconSelected: <BookmarkFill />,
     title: 'Bookmark',
     notify: 0
   },
   {
     key: 'lists',
     icon: <Lists />,
+    iconSelected: <ListsFill />,
     title: 'Lists',
     notify: 0
   },
   {
     key: 'profile',
     icon: <Profile />,
+    iconSelected: <ProfileFill />,
     title: 'Profile',
     notify: 0
   },
   {
     key: 'more',
     icon: <More />,
+    iconSelected: <More />,
     title: 'More',
     notify: 0
   }
@@ -74,19 +90,20 @@ const MENU = [
 
 import styles from './navigation.module.css'
 
-function Navigation({ flat = false, selectedKey }) {
+function Navigation({ flat = false, selectedKey = 'home' }) {
   return (
     <nav className={styles.nav}>
       {MENU.map((menu) => {
         const showTitle = !flat && menu.title.length > 0
-        
+        const selected = selectedKey === menu.key
+
         return (
           <NavigationButton
             key={menu.key}
             notify={menu.notify}
             selected={selectedKey === menu.key}
           >
-            {menu.icon}
+            {selected ? menu.iconSelected : menu.icon}
             {showTitle && <TextTitle>{menu.title}</TextTitle>}
           </NavigationButton>
         )
