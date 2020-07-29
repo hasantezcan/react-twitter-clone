532ad8c4124c3dd50cfffa02e5412191341d3f6e
hasantezcan <hasantezcan77@gmail.com>
2020-07-29 21:06:29 +0300

    Add theme select feature

---
+++ components/theme-select.js
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react'
+
+import styles from './theme-select.module.css'
+import StoreContext from '../store'
+
+const THEME = {
+  light: 'Light',
+  dim: 'Dim',
+  dark: 'Dark'
+}
+
+function ThemeSelect() {
+  // const [selectedTheme, selectedThemeSet] = useState('light')
+  const store = useContext(StoreContext)
+
+  return (
+    <div className={styles.container}>
+      {['light', 'dim', 'dark'].map((theme) => (
+        <label className={styles.label}>
+          <input
+            type="radio"
+            value={theme}
+            name={theme}
+            checked={theme === store.theme}
+            onChange={(e) => store.changeTheme(e.target.value)}
+          />
+          {THEME[theme]}
+        </label>
+      ))}
+    </div>
+  )
+}
+
+export default ThemeSelect
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect, useLayoutEffect } from 'react'
 
 import '../styles/app.css'
+import StoreContext from '../store'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [theme, themeSet] = useState(null)
+
+  useLayoutEffect(() => {
+    const theme = localStorage.getItem('THEME') || 'light'
+    themeSet(theme)
+  }, [])
+
+  const changeTheme = (theme) => {
+    themeSet(theme)
+    localStorage.setItem('THEME', theme)
+  }
+
+  useEffect(() => {
+    if (!theme) return
+    const $html = document.querySelector('html')
+    $html.setAttribute('class', theme)
+  }, [theme])
+
+  return (
+    <StoreContext.Provider value={{ theme, changeTheme }}>
+      <Component {...pageProps} />
+    </StoreContext.Provider>
+  )
 }
